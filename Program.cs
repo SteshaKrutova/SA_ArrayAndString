5030aa309135f6671272cb02cd6919ede0f59cae
Krutova Stefania <mira.kru@yandex.ru>
2024-05-02 23:34:45 +0300

    Zadanie1

--- Program.cs
+++ Program.cs
@@ -4,17 +4,39 @@
 char[,] CreateMatrix(int len){
     char[,] Matrix = new char[len,len];
     string token = string.Empty;
+    Console.WriteLine("Please, write " +len*len+ " token: ");
     for (int i=0; i<len;i++){
         for (int j=0; j<len;j++){
-            string str = "A";
+            string str = Console.ReadLine();
             char character = char.Parse(str);
             Matrix[i,j]=character;
     }
     }
     return Matrix;
 }
-PrintMatrix(CreateMatrix(3));
 
+void Zadanie1(){
+    Console.WriteLine("Pleaze, write someone nember (for example '2' or '3')");
+    int len = Convert.ToInt32(Console.ReadLine());
+    char[,] Matrix = new char[len,len];
+    Matrix = CreateMatrix(len);
+    Console.WriteLine();
+    Console.WriteLine("A matrix for yours tokens:");
+    PrintMatrix(Matrix);
+    Console.WriteLine();
+    Console.WriteLine("A line for yours tokens:");
+    PrintString(MatrixToString(Matrix));
+}
+
+string MatrixToString (char[,] Matrix){
+    string result = string.Empty;
+    for (int i=0; i<Matrix.GetLength(0);i++){
+        for (int j=0; j<Matrix.GetLength(1);j++){
+            result+=(Matrix[i, j]+" ");   
+    }
+    }
+    return result;
+}
 
 
 void PrintMatrix(char[,] Matrix){
@@ -26,3 +48,11 @@ void PrintMatrix(char[,] Matrix){
     }
 }
 
+void PrintString(string str){
+    for (int i=0; i<str.Length;i++){
+        Console.Write(str[i]+" ");   
+    }
+    
+}
+
+Zadanie1();
